ce88671cc844211c4ae07f8d32ac7d08a112c909
vicktorl <v.lenam01@ufromail.cl>
2023-07-13 10:59:58 -0400

    se crea clase Corsconfig y aalgunas pruebas TDD

---
+++ src/test/java/ufro/examen/utils/TDDTest.java
@@ -0,0 +1,29 @@
+package ufro.examen.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TDDTest {
+    Producto producto;
+    Bodega bodega;
+
+    @BeforeEach
+    void setUp(){
+        producto = new Producto();
+        bodega = new Bodega();
+    }
+    @Test
+    @DisplayName("Test nombre producto")
+    public void testLargoMsje(){
+    String principioActivo ="salbutamol";
+        Assertions.assertTrue(producto.largoTexto(principioActivo));
+    }
+    @Test
+    @DisplayName("ajustar saldo")
+    public void testSaldo(){
+        Integer saldo =3000;
+        Assertions.assertTrue(producto.getSaldo(saldo));
+    }
+}
